501c56e4d30fcb51f7557cc164448079f2370dd0
SachinDmv <sachin.dmvs@gmail.com>
2021-12-22 17:01:17 +0530

    practice commit

---
+++ src/test/java/practice/Demo.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Demo {
+public static void main(String[] args) {
+	int input=2;
+	if(input>1)
+	{
+		System.out.println("Hello");
+	}else {
+		System.out.println("Hi");
+	}
+}
+}
